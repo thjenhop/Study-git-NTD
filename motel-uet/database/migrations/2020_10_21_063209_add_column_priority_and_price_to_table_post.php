217f693fac27ea112eb21fe1e9dbeab486ee28f1
hopdt1404 <hopdt1404@gmail.com>
2020-10-21 13:35:14 +0700

    add_column_priority_and_price_to_table_post

---
+++ motel-uet/database/migrations/2020_10_21_063209_add_column_priority_and_price_to_table_post.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnPriorityAndPriceToTablePost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedTinyInteger('priority_post')->nullable(false)->default(0)->comment('Độ ưu tiên của bài viết');
+            $table->unsignedDouble('post_price', 15, 3)->nullable(false)->default(0)->comment('Gía đăng bài viết');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
