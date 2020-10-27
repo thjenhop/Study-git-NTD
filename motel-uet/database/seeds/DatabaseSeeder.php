b0d2b1f8389fd3f38fbe8fc12748c4faf3981cbf
hopdt1404 <hopdt1404@gmail.com>
2020-10-27 14:05:01 +0700

    create categories data seed

--- motel-uet/database/seeds/DatabaseSeeder.php
+++ motel-uet/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              GroupUserSeeder::class,
+             CategoriesSeeder::class,
 //             FakeUserSeeder::class,
          ]);
     }
